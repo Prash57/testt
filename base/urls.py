3dc8754187059fd4833b01b488970ff56044fd98
Roshan Dahal <58673204+the-roshandahal@users.noreply.github.com>
2024-03-30 22:31:13 +1100

    urls

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 from .views import custom_404_view
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.views.static import serve
+from django.urls import re_path
+
+
 
 urlpatterns = [
     # login routes
@@ -121,6 +129,12 @@ urlpatterns = [
     # dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
 
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ]
 
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = custom_404_view
--- school/settings.py
+++ school/settings.py
@@ -144,14 +144,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/img/'
-
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / "static",
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
